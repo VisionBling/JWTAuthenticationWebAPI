f30fe7742068f95d34a6f2308e16e2390b5ea32a
Uloma Okenyi <cyndibilo@gmail.com>
2022-03-10 04:53:18 +0000

    modified JWT and swagger configuration also added todo controller implementation

---
+++ JWTAuthWebAPI/JWTAuthWebAPI/Controllers/TodoItemController.cs
@@ -0,0 +1,59 @@
+﻿using JWTAuth.Data.Entities;
+using JWTAuthWebAPI.Models;
+using JWTAuthWebAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoItemController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        // GET: api/TodoItem
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "admin,user")]
+        public async Task<ActionResult<IEnumerable<TodoItemModel>>> GetTodoItems()
+        {
+            return Ok(await _todoItemService.GetAllItemsAsync());
+        }
+
+        // GET: api/TodoItem/5
+        [HttpGet("{id}")]
+        [Authorize(Roles = "admin,user")] // Accessible by both admin and user
+        public async Task<ActionResult<TodoItemModel>> GetTodoItem(int id)
+        {
+            var todoItem = await _todoItemService.GetItemByIdAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")] // Restricted to admin
+        public async Task<ActionResult<TodoItemModel>> PostTodoItem([FromBody] TodoItemModel item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdItem = await _todoItemService.CreateItemAsync(item);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = createdItem.Id }, createdItem);
+        }
+    }
+}
--- JWTAuthWebAPI/JWTAuthWebAPI/Program.cs
+++ JWTAuthWebAPI/JWTAuthWebAPI/Program.cs
@@ -1,9 +1,12 @@
+using JWTAuth.Data.Entities;
 using JWTAuthWebAPI.Data;
 using JWTAuthWebAPI.Services;
 using JWTAuthWebAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +14,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITodoItemService, TodoItemService>();
 
 // Add this line if you're using Entity Framework Core
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Configure JWT Authentication
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -39,7 +47,39 @@ builder.Services.AddAuthorization();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Configure Swagger to use the Bearer Token
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -47,14 +87,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting(); 
+
 // Configure the HTTP request pipeline.
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers(); 
+});
 
 app.MapControllers();
 
